886c12ce484d1d1f1f9d2d7dc6f1771c2f70b01c
Emily Danielle Brockett <emilybrockett@gmail.com>
2023-11-15 14:45:33 -0600

    initial commit - moved files from other app

---
+++ src/components/TitleBanner.jsx
@@ -0,0 +1,11 @@
+import "../styles/TitleBanner.css";
+
+const TitleBanner = () => {
+  return (
+    <div className="bannerBackground">
+      <h1 className="mainTitle">React GitHub Deployment Instructions</h1>
+    </div>
+  );
+};
+
+export default TitleBanner;
