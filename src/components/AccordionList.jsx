fb45134f54cb5adc9b28fb5ad1bfb9cb5a3a3652
Emily Danielle Brockett <emilybrockett@gmail.com>
2023-11-15 17:32:16 -0600

    added images and further information for the lesson

--- src/components/AccordionList.jsx
+++ src/components/AccordionList.jsx
@@ -14,7 +14,7 @@ const AccordionList = () => {
               aria-expanded="true"
               aria-controls="panelsStayOpen-collapseOne"
             >
-              Accordion Item #1
+              Instructions Part 1: create-react-app
             </button>
           </h2>
           <div
@@ -22,14 +22,24 @@ const AccordionList = () => {
             className="accordion-collapse collapse show"
           >
             <div className="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              <strong>Set up your development environment</strong>
+              <ul>
+                <li>
+                  Make sure you have Node.js and npm installed on your computer.
+                </li>
+                <li>
+                  create your react app using{" "}
+                  <strong>npx create-react-app my-app</strong>
+                </li>
+                <li>
+                  connect your new react app to a new github repository. If you
+                  don't remember how to create a new repo, then go to{" "}
+                  <a href="https://docs.google.com/document/d/1mjjqzJUH4-exiDMGnrgIrM1g_A4a8dLv6nNlMFhfnqA/edit?usp=sharing">
+                    Setting Up GitHub
+                  </a>{" "}
+                  for a refresher.
+                </li>
+              </ul>
             </div>
           </div>
         </div>
@@ -43,7 +53,7 @@ const AccordionList = () => {
               aria-expanded="false"
               aria-controls="panelsStayOpen-collapseTwo"
             >
-              Accordion Item #2
+              Instructions Part 2: Build it
             </button>
           </h2>
           <div
@@ -51,7 +61,111 @@ const AccordionList = () => {
             className="accordion-collapse collapse"
           >
             <div className="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is
+              <strong>Build your Project 3</strong>
+              <ul>
+                <li>
+                  Build your project, committing often, and pushing to your
+                  GitHub repo.
+                </li>
+                <li>
+                  Once you feel good about your react app, then use{" "}
+                  <strong>npm run build</strong> command to create an optimized
+                  production build.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseThree"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseThree"
+            >
+              Instructions Part 3: Install Github Pages
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseThree"
+            className="accordion-collapse collapse"
+          >
+            <div className="accordion-body">
+              <strong>This is a very important step.</strong>
+              <ul>
+                <li>
+                  use the command{" "}
+                  <strong>npm install gh-pages --save-dev</strong>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseFour"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseFour"
+            >
+              Instructions Part 4: Configuring package.json
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseFour"
+            className="accordion-collapse collapse"
+          >
+            <div className="accordion-body">
+              <strong>
+                Any time you update your project after deployment, you will need
+                to reconfigure here.
+              </strong>
+              <ul>
+                <li>
+                  To configure your package.json file, open the file and add the
+                  following information:
+                </li>
+                <img
+                  style={{ height: 200, width: 550 }}
+                  src="../homepage-packageJSON.png"
+                  sr-only="homepage configuration: homepage: your-username.github.io/your-repo-name"
+                ></img>
+                <br />
+                <br />
+                <img
+                  style={{ height: 200, width: 300 }}
+                  src="../scripts-packageJSON.png"
+                  sr-only="scripts configuration: scripts configuration picture for predeployment and deployment"
+                ></img>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseFive"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseFive"
+            >
+              Instructions Part 5: Run Deploy
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseFive"
+            className="accordion-collapse collapse"
+          >
+            <div className="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
               hidden by default, until the collapse plugin adds the appropriate
               classNamees that we use to style each element. These classNamees
               control the overall appearance, as well as the showing and hiding
@@ -68,15 +182,44 @@ const AccordionList = () => {
               className="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseThree"
+              data-bs-target="#panelsStayOpen-collapseSix"
               aria-expanded="false"
-              aria-controls="panelsStayOpen-collapseThree"
+              aria-controls="panelsStayOpen-collapseSix"
             >
-              Accordion Item #3
+              Instructions Part 6: Enable GitHub Pages
             </button>
           </h2>
           <div
-            id="panelsStayOpen-collapseThree"
+            id="panelsStayOpen-collapseSix"
+            className="accordion-collapse collapse"
+          >
+            <div className="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is
+              hidden by default, until the collapse plugin adds the appropriate
+              classNamees that we use to style each element. These classNamees
+              control the overall appearance, as well as the showing and hiding
+              via CSS transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseSeven"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseSeven"
+            >
+              Instructions Part 7: Access Your App
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseSeven"
             className="accordion-collapse collapse"
           >
             <div className="accordion-body">
--- src/components/TitleBanner.jsx
+++ src/components/TitleBanner.jsx
@@ -3,7 +3,7 @@ import "../styles/TitleBanner.css";
 const TitleBanner = () => {
   return (
     <div className="bannerBackground">
-      <h1 className="mainTitle">React GitHub Deployment Instructions</h1>
+      <h1 className="mainTitle">Deploy React on GitHub Pages Instructions</h1>
     </div>
   );
 };
